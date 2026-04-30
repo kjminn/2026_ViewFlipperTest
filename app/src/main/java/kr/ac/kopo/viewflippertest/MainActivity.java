61c2a99a90b6629a51f6b0ad4e9f7a49a1f430e1
kjminn <kjminn2001@gmail.com>
2026-04-30 16:22:46 +0900

    ViewFlipper 실습

---
+++ app/src/main/java/kr/ac/kopo/viewflippertest/MainActivity.java
@@ -0,0 +1,44 @@
+package kr.ac.kopo.viewflippertest;
+
+import android.os.Bundle;import android.view.View;import android.widget.Button;import android.widget.ViewFlipper;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    ViewFlipper flipper;
+    Button btnPrev, btnNext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btnPrev = findViewById(R.id.btn_prev);
+        btnNext = findViewById(R.id.btn_next);
+        flipper = findViewById(R.id.flipper);
+
+        btnPrev.setOnClickListener(btnListener);
+        btnNext.setOnClickListener(btnListener);
+    }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+    @Override
+        public void onClick(View v) {
+            Button btnEvent = (Button)v;
+            if (btnEvent == btnPrev)
+                flipper.showPrevious();
+            else
+                flipper.showNext();
+        }
+    };
+}
